a165d9a71e3b169b292538448002d02bc0980a69
amcmilne <amcmilne@gmail.com>
2021-01-26 17:39:46 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 const mysql = require("mysql");
-connection = mysql.createConnection({
-    host: "localhost",
-    user: "webapi",
-    password: "password123",
-    database: "burgers_db", 
-});
-connection.connect(function(err){
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "webapi",
+        password: "password123",
+        database: "burgers_db",
+    });
+};
+connection.connect(function (err) {
     if (err) {
         console.log("error connecting: " + err.stack);
-        return; 
+        return;
     }
     console.log("connected as id " + connection.threadId);
 });
